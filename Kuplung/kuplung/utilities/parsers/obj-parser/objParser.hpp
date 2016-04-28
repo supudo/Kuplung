8a6cb4db68486627f734f619044c6a78251b9898
Sergey Petrov <supudo@gmail.com>
2016-04-28 16:23:11 +0300

    rev

--- Kuplung/kuplung/utilities/parsers/obj-parser/objParser.hpp
+++ Kuplung/kuplung/utilities/parsers/obj-parser/objParser.hpp
@@ -43,16 +43,6 @@ private:
     void loadMaterialFile(std::string materialFile);
     MeshMaterialTextureImage parseTextureImage(std::string textureLine);
     std::vector<std::string> splitString(const std::string &s, std::string delimiter);
-
-    void fixIndices(
-        std::vector<glm::vec3> & in_vertices,
-        std::vector<glm::vec2> & in_uvs,
-        std::vector<glm::vec3> & in_normals,
-        std::vector<unsigned int> & out_indices,
-        std::vector<glm::vec3> & out_vertices,
-        std::vector<glm::vec2> & out_uvs,
-        std::vector<glm::vec3> & out_normals
-    );
     bool getSimilarVertexIndex(PackedVertex & packed, std::map<PackedVertex, unsigned int> & vertexToOutIndex, unsigned int & result);
 
     // current object name
