9390c5cf7928f10a040ce8a1bf4f2c5396bf818a
Sergey Petrov <supudo@gmail.com>
2016-07-05 18:59:43 +0300

    rev

--- Kuplung/kuplung/meshes/scene/ModelFaceDeferredT.hpp
+++ Kuplung/kuplung/meshes/scene/ModelFaceDeferredT.hpp
@@ -24,47 +24,26 @@ public:
     void render(glm::mat4 matrixProjection, glm::mat4 matrixCamera, glm::mat4 matrixModel, glm::vec3 vecCameraPosition, WorldGrid *grid, glm::vec3 uiAmbientLight);
 
 private:
-    void renderGeometryPass();
-    void renderLightingPass();
-    void renderLightBox();
-    void renderQuad();
-    void renderCube();
 
+    GLuint shaderProgram_GeometryPass, shaderProgram_LightingPass, shaderProgram_LightBox;
+    GLuint glVAO, vboVertices, vboNormals, vboTextureCoordinates, vboIndices;
+    GLuint gl_TextureDiffuse, gl_TextureSpecular;
+
+    GLuint gBuffer;
+    GLuint gPosition, gNormal, gAlbedoSpec;
+
+    const GLuint NR_LIGHTS = 32;
+    std::vector<glm::vec3> objectPositions;
     std::vector<glm::vec3> lightPositions;
     std::vector<glm::vec3> lightColors;
 
-    const GLuint NR_LIGHTS = 32;
+    GLuint quadVAO = 0;
+    GLuint quadVBO;
+    void renderQuad();
 
-    // Model
-    GLuint glVAO;
-    GLuint vboVertices, vboNormals, vboTextureCoordinates, vboIndices, vboTangents, vboBitangents;
-
-    // G-Buffer
-    GLuint gBuffer, rboDepth;
-    GLuint gPosition, gNormal, gAlbedoSpec;
-
-    // Geometry Pass
-    GLuint shaderProgram_GeometryPass;
-    GLuint gl_GeometryPass_Position, gl_GeometryPass_Normal, gl_GeometryPass_TextureCoordinate;
-    GLint gl_GeometryPass_ProjectionMatrix, gl_GeometryPass_ViewMatrix, gl_GeometryPass_ModelMatrix;
-    GLuint gl_GeometryPass_TextureDiffuse, gl_GeometryPass_TextureSpecular;
-
-    // Lighting Pass
-    std::vector<ModelDeferred_LightSource> lightSources;
-    GLuint shaderProgram_LightingPass;
-    GLint gl_LightingPass_VPosition, gl_LightingPass_VTexCoords;
-    GLint gl_LightingPass_Position, gl_LightingPass_Normal, gl_LightingPass_AlbedoSpec;
-    GLint gl_LightingPass_ViewPosition, gl_LightingPass_DrawMode;
-
-    // Light Box
-    GLuint shaderProgram_LightBox;
-    GLint gl_LightBox_ProjectionMatrix, gl_LightBox_ViewMatrix, gl_LightBox_ModelMatrix, gl_LightBox_LightColor;
-
-    // Quad
-    GLuint vaoQuad, vboQuad;
-
-    // Cube
-    GLuint vaoCube, vboCube;
+    GLuint cubeVAO = 0;
+    GLuint cubeVBO = 0;
+    void renderCube();
 };
 
 #endif /* ModelFaceDeferredT_hpp */
