daec9c2870d523560eee8f00a2db82676ae01c24
supudo <supudo@gmail.com>
2017-11-14 16:59:46 +0200

    KuplungIDE wip

--- Kuplung/kuplung/ui/components/ide/KuplungIDE.cpp
+++ Kuplung/kuplung/ui/components/ide/KuplungIDE.cpp
@@ -25,27 +25,77 @@ void KuplungIDE::init() {
 	auto lang = TextEditor::LanguageDefinition::GLSL();
 
 	static const char* keywords[] = {
-		"attribute", "const", "uniform", "varying", "layout", "centroid", "flat", "smooth", "noperspective", "patch", "sample", "break",
-		"continue", "do", "for", "while", "switch", "case", "default", "if", "else", "subroutine", "in", "out", "inout", "float", "double",
-		"int", "void", "bool", "true", "false", "invariant", "discard", "return", "mat2", "mat3", "mat4", "dmat2", "dmat3", "dmat4",
-		"mat2x2", "mat2x3", "mat2x4", "dmat2x2", "dmat2x3", "dmat2x4", "mat3x2", "mat3x3", "mat3x4", "dmat3x2", "dmat3x3", "dmat3x4",
-		"mat4x2", "mat4x3", "mat4x4", "dmat4x2", "dmat4x3", "dmat4x4", "vec2", "vec3", "vec4", "ivec2", "ivec3", "ivec4", "bvec2",
-		"bvec3", "bvec4", "dvec2", "dvec3", "dvec4", "uint", "uvec2", "uvec3", "uvec4", "lowp", "mediump", "highp", "precision",
-		"sampler1D", "sampler2D", "sampler3D", "samplerCube", "sampler1DShadow", "sampler2DShadow", "samplerCubeShadow", "sampler1DArray",
-		"sampler2DArray", "sampler1DArrayShadow", "sampler2DArrayShadow", "isampler1D", "isampler2D", "isampler3D", "isamplerCube",
-		"isampler1DArray", "isampler2DArray", "usampler1D", "usampler2D", "usampler3D", "usamplerCube", "usampler1DArray",
-		"usampler2DArray", "sampler2DRect", "sampler2DRectShadow", "isampler2DRect", "usampler2DRect", "samplerBuffer", "isamplerBuffer",
-		"usamplerBuffer", "sampler2DMS", "isampler2DMS", "usampler2DMS", "sampler2DMSArray", "isampler2DMSArray", "usampler2DMSArray",
-		"samplerCubeArray", "samplerCubeArrayShadow", "isamplerCubeArray", "usamplerCubeArray", "struct", "common", "partition", "active",
-		"asm", "class", "union", "enum", "typedef", "template", "this", "packed", "goto", "inline", "noinline", "volatile", "public",
-		"static", "extern", "external", "interface", "long", "short", "half", "fixed", "unsigned", "superp", "input", "output", "hvec2",
-		"hvec3", "hvec4", "fvec2", "fvec3", "fvec4", "sampler3DRect", "filter", "image1D", "image2D", "image3D", "imageCube", "iimage1D",
-		"iimage2D", "iimage3D", "iimageCube", "uimage1D", "uimage2D", "uimage3D", "uimageCube", "image1DArray", "image2DArray",
-		"iimage1DArray", "iimage2DArray", "uimage1DArray", "uimage2DArray", "image1DShadow", "image2DShadow", "image1DArrayShadow",
-		"image2DArrayShadow", "imageBuffer", "iimageBuffer", "uimageBuffer", "sizeof", "cast", "namespace", "using", "row_major"
+		// types
+		"float", "double", "int", "void", "bool", "true", "false", "mat2", "mat3", "mat4", "dmat2", "dmat3", "dmat4", 
+		"mat2x2", "mat2x3", "mat2x4", "dmat2x2", "dmat2x3", "dmat2x4", "mat3x2", "mat3x3", "mat3x4", "dmat3x2", 
+		"dmat3x3", "dmat3x4", "mat4x2", "mat4x3", "mat4x4", "dmat4x2", "dmat4x3", "dmat4x4", "vec2", "vec3", "vec4", 
+		"ivec2", "ivec3", "ivec4", "bvec2", "bvec3", "bvec4", "dvec2", "dvec3", "dvec4", "uint", "uvec2", "uvec3", 
+		"uvec4", "sampler1D", "sampler2D", "sampler3D", "samplerCube", "sampler1DShadow", "sampler2DShadow", 
+		"samplerCubeShadow", "sampler1DArray", "sampler2DArray", "sampler1DArrayShadow", "sampler2DArrayShadow", 
+		"isampler1D", "isampler2D", "isampler3D", "isamplerCube", "isampler1DArray", "isampler2DArray", "usampler1D",
+		"usampler2D", "usampler3D", "usamplerCube", "usampler1DArray", "usampler2DArray", "sampler2DRect", 
+		"sampler2DRectShadow", "isampler2DRect", "usampler2DRect", "samplerBuffer", "isamplerBuffer", "usamplerBuffer",
+		"sampler2DMS", "isampler2DMS", "usampler2DMS", "sampler2DMSArray", "isampler2DMSArray", "usampler2DMSArray", 
+		"samplerCubeArray", "samplerCubeArrayShadow", "isamplerCubeArray", "usamplerCubeArray", "image1D", "iimage1D", 
+		"uimage1D", "image2D", "iimage2D", "uimage2D", "image3D", "iimage3D", "uimage3D", "image2DRect", 
+		"iimage2DRect", "uimage2DRect", "imageCube", "iimageCube", "uimageCube", "imageBuffer", "iimageBuffer",
+		"uimageBuffer", "image1DArray", "iimage1DArray", "uimage1DArray", "image2DArray", "iimage2DArray", "uimage2DArray", 
+		"imageCubeArray", "iimageCubeArray", "uimageCubeArray", "image2DMS", "iimage2DMS", "uimage2DMS", "image2DMSArray",
+		"iimage2DMSArray", "uimage2DMSArray", "long", "short", "half", "fixed", "unsigned", "hvec2", "hvec3", "hvec4",
+		"fvec2", "fvec3", "fvec4", "sampler3DRect",
+		// modifiers
+		"attribute", "const", "uniform", "varying", "buffer", "shared", "coherent", "volatile", "restrict", "readonly",
+		"writeonly", "atomic_uint", "layout", "centroid", "flat", "smooth", "noperspective", "patch", "sample", "break",
+		"continue", "do", "for", "while", "switch", "case", "default", "if", "else", "subroutine", "in", "out", "inout",
+		"invariant", "discard", "return", "lowp", "mediump", "highp", "precision", "struct", "common", "partition",
+		"active", "asm", "class", "union", "enum", "typedef", "template", "this", "packed", "resource", "goto", "inline",
+		"noinline", "public", "static", "extern", "external", "interface", "superp", "input", "output", "filter",
+		"sizeof", "cast", "namespace", "using", "row_major", "early_fragment_tests", "varying", "attribute",
+		// deprecated
+		"gl_FragColor", "gl_FragData", "gl_MaxVarying", "gl_MaxVaryingFloats", "gl_MaxVaryingComponents",
+		//
+		"shared", "packed", "std140", "row_major", "column_major", "ccw", "cw", "equal_spacing", "fractional_even_spacing", 
+		"fractional_odd_spacing", "index", "invocations", "isolines", "line_strip", "lines", "lines_adjacency", "location",
+		"binding", "max_vertices", "origin_upper_left", "pixel_center_integer", "point_mode", "points", "quads", "stream",
+		"triangle_strip", "triangles", "triangles_adjacency", "vertices", "local_size_x", "local_size_y", "local_size_z",
+		"std430"
 	};
 	for (auto i : keywords)
 		lang.mKeywords.insert(i);
+
+
+	static const char* const identifiers[] = {
+		"abs", "acos", "acosh", "all", "any", "asin", "asinh", "atan", "atanh", "atomicCounter", "atomicCounterDecrement",
+		"atomicCounterIncrement", "barrier", "bitCount", "bitfieldExtract", "bitfieldInsert", "bitfieldReverse", "ceil",
+		"clamp", "cos", "cosh", "cross", "degrees", "determinant", "dFdx", "dFdy", "dFdyFine", "dFdxFine", "dFdyCoarse",
+		"dFdxCourse", "fwidthFine", "fwidthCoarse", "distance", "dot", "EmitStreamVertex", "EmitVertex", "EndPrimitive",
+		"EndStreamPrimitive", "equal", "exp", "exp2", "faceforward", "findLSB", "findMSB", "floatBitsToInt", "floatBitsToUint",
+		"floor", "fma", "fract", "frexp", "fwidth", "greaterThan", "greaterThanEqual", "imageAtomicAdd", "imageAtomicAnd",
+		"imageAtomicCompSwap", "imageAtomicExchange", "imageAtomicMax", "imageAtomicMin", "imageAtomicOr", "imageAtomicXor",
+		"imageLoad", "imageSize", "imageStore", "imulExtended", "intBitsToFloat", "imageSamples", "interpolateAtCentroid",
+		"interpolateAtOffset", "interpolateAtSample", "inverse", "inversesqrt", "isinf", "isnan", "ldexp", "length", "lessThan",
+		"lessThanEqual", "log", "log2", "matrixCompMult", "max", "memoryBarrier", "min", "mix", "mod", "modf", "noise",
+		"normalize", "not", "notEqual", "outerProduct", "packDouble2x32", "packHalf2x16", "packSnorm2x16", "packSnorm4x8",
+		"packUnorm2x16", "packUnorm4x8", "pow", "radians", "reflect", "refract", "round", "roundEven", "sign", "sin", "sinh",
+		"smoothstep", "sqrt", "step", "Tan", "tanh", "texelFetch", "texelFetchOffset", "texture", "textureGather",
+		"textureGatherOffset", "textureGatherOffsets", "textureGrad", "textureGradOffset", "textureLod", "textureLodOffset",
+		"textureOffset", "textureProj", "textureProjGrad", "textureProjGradOffset", "textureProjLod", "textureProjLodOffset",
+		"textureProjOffset", "textureQueryLevels", "textureQueryLod", "textureSize", "transpose", "trunc", "uaddCarry",
+		"uintBitsToFloat", "umulExtended", "unpackDouble2x32", "unpackHalf2x16", "unpackSnorm2x16", "unpackSnorm4x8",
+		"unpackUnorm2x16", "unpackUnorm4x8", "usubBorrow",
+		// deprecated
+		"texture1D", "texture1DProj", "texture1DLod", "texture1DProjLod", "texture2D", "texture2DProj", "texture2DLod", "texture2DProjLod",
+		"texture2DRect", "texture2DRectProj", "texture3D", "texture3DProj", "texture3DLod", "texture3DProjLod", "shadow1D", "shadow1DProj",
+		"shadow1DLod", "shadow1DProjLod","shadow2D", "shadow2DProj", "shadow2DLod", "shadow2DProjLod", "textureCube", "textureCubeLod"
+	};
+	for (auto& k : identifiers)
+		lang.mIdentifiers.insert(k);
+
+	static const char* ppnames[] = { 
+		"define", "undef", "if", "ifdef", "ifndef", "else", "elif", "endif", "error", "pragma", "extension", "version", "line"
+	};
+	for (auto i : ppnames)
+		lang.mPreprocIdentifiers.insert(i);
 
 	this->kuplungEditor.SetLanguageDefinition(lang);
 }
@@ -54,7 +104,10 @@ void KuplungIDE::draw(const char* title, bool* p_opened, std::vector<ModelFaceBa
     ImGui::SetNextWindowSize(ImVec2(Settings::Instance()->frameLog_Width, Settings::Instance()->frameLog_Height), ImGuiSetCond_FirstUseEver);
     ImGui::SetNextWindowPos(ImVec2(40, 40), ImGuiSetCond_FirstUseEver);
 
-	ImGui::Begin(title, p_opened, ImGuiWindowFlags_ShowBorders);
+	auto cpos = this->kuplungEditor.GetCursorPosition();
+	ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(.13f, .13f, .13f, 1.0f));
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+	ImGui::Begin(title, p_opened, ImGuiWindowFlags_ShowBorders | ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_AlwaysVerticalScrollbar);
 
 	if (meshModelFaces.size() > 0) {
 		std::vector<ModelFaceBase*>::const_iterator faceIterator;
@@ -87,7 +140,6 @@ void KuplungIDE::draw(const char* title, bool* p_opened, std::vector<ModelFaceBa
 	if (ImGui::BeginMenuBar())
 		ImGui::EndMenuBar();
 
-	auto cpos = this->kuplungEditor.GetCursorPosition();
 	ImGui::Text("%6d/%-6d %6d lines  %s %s | %s | %s", cpos.mLine + 1, cpos.mColumn + 1, this->kuplungEditor.GetTotalLines(),
 		this->kuplungEditor.IsOverwrite() ? "Ovr" : "Ins",
 		this->kuplungEditor.CanUndo() ? "*" : " ",
@@ -96,6 +148,8 @@ void KuplungIDE::draw(const char* title, bool* p_opened, std::vector<ModelFaceBa
 	this->kuplungEditor.Render("Kuplung IDE");
 
     ImGui::End();
+	ImGui::PopStyleVar();
+	ImGui::PopStyleColor();
 }
 
 void KuplungIDE::loadSelectedShader() {
@@ -109,6 +163,8 @@ void KuplungIDE::loadSelectedShader() {
 			shaderFile = Settings::Instance()->appFolder() + "/shaders/model_face.tcs";
 		else if (this->selectedIndex == 3)
 			shaderFile = Settings::Instance()->appFolder() + "/shaders/model_face.tes";
+		else if (this->selectedIndex == 4)
+			shaderFile = Settings::Instance()->appFolder() + "/shaders/model_face.frag";
 		std::ifstream t(shaderFile);
 		std::string str((std::istreambuf_iterator<char>(t)),
 			std::istreambuf_iterator<char>());
--- Kuplung/kuplung/utilities/imgui/ImGuiColorTextEdit/TextEditor.cpp
+++ Kuplung/kuplung/utilities/imgui/ImGuiColorTextEdit/TextEditor.cpp
@@ -1588,25 +1588,9 @@ TextEditor::LanguageDefinition TextEditor::LanguageDefinition::GLSL()
 	if (!inited)
 	{
 		static const char* const keywords[] = {
-			"attribute", "const", "uniform", "varying", "layout", "centroid", "flat", "smooth", "noperspective", "patch", "sample", "break",
-			"continue", "do", "for", "while", "switch", "case", "default", "if", "else", "subroutine", "in", "out", "inout", "float", "double",
-			"int", "void", "bool", "true", "false", "invariant", "discard", "return", "mat2", "mat3", "mat4", "dmat2", "dmat3", "dmat4",
-			"mat2x2", "mat2x3", "mat2x4", "dmat2x2", "dmat2x3", "dmat2x4", "mat3x2", "mat3x3", "mat3x4", "dmat3x2", "dmat3x3", "dmat3x4",
-			"mat4x2", "mat4x3", "mat4x4", "dmat4x2", "dmat4x3", "dmat4x4", "vec2", "vec3", "vec4", "ivec2", "ivec3", "ivec4", "bvec2",
-			"bvec3", "bvec4", "dvec2", "dvec3", "dvec4", "uint", "uvec2", "uvec3", "uvec4", "lowp", "mediump", "highp", "precision",
-			"sampler1D", "sampler2D", "sampler3D", "samplerCube", "sampler1DShadow", "sampler2DShadow", "samplerCubeShadow", "sampler1DArray",
-			"sampler2DArray", "sampler1DArrayShadow", "sampler2DArrayShadow", "isampler1D", "isampler2D", "isampler3D", "isamplerCube",
-			"isampler1DArray", "isampler2DArray", "usampler1D", "usampler2D", "usampler3D", "usamplerCube", "usampler1DArray",
-			"usampler2DArray", "sampler2DRect", "sampler2DRectShadow", "isampler2DRect", "usampler2DRect", "samplerBuffer", "isamplerBuffer",
-			"usamplerBuffer", "sampler2DMS", "isampler2DMS", "usampler2DMS", "sampler2DMSArray", "isampler2DMSArray", "usampler2DMSArray",
-			"samplerCubeArray", "samplerCubeArrayShadow", "isamplerCubeArray", "usamplerCubeArray", "struct", "common", "partition", "active",
-			"asm", "class", "union", "enum", "typedef", "template", "this", "packed", "goto", "inline", "noinline", "volatile", "public",
-			"static", "extern", "external", "interface", "long", "short", "half", "fixed", "unsigned", "superp", "input", "output", "hvec2",
-			"hvec3", "hvec4", "fvec2", "fvec3", "fvec4", "sampler3DRect", "filter", "image1D", "image2D", "image3D", "imageCube", "iimage1D",
-			"iimage2D", "iimage3D", "iimageCube", "uimage1D", "uimage2D", "uimage3D", "uimageCube", "image1DArray", "image2DArray",
-			"iimage1DArray", "iimage2DArray", "uimage1DArray", "uimage2DArray", "image1DShadow", "image2DShadow", "image1DArrayShadow",
-			"image2DArrayShadow", "imageBuffer", "iimageBuffer", "uimageBuffer", "sizeof", "cast", "namespace", "using", "row_major",
-			"gl_in", "EmitVertex", "EndPrimitive"
+			"auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "inline", "int", "long", "register", "restrict", "return", "short",
+			"signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while", "_Alignas", "_Alignof", "_Atomic", "_Bool", "_Complex", "_Generic", "_Imaginary",
+			"_Noreturn", "_Static_assert", "_Thread_local"
 		};
 		for (auto& k : keywords)
 			langDef.mKeywords.insert(k);
